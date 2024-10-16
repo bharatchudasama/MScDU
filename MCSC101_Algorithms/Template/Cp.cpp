79fabb55f917fc1efe6349d14e4f14ff8d08142a
Jishnu Bandyopadhyay <therealjishnu@gmail.com>
2024-10-16 19:04:31 +0530

    Create Cp.cpp
    Short and general Template for competitive programing and DSA practice

---
+++ MCSC101_Algorithms/Template/Cp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    
+}
+
+int main() {
+	// your code goes here
+	ll t;
+	cin>>t;
+	while(t--){
+	    solve();
+	    if(t) cout << endl;
+	}
+	
+	return 0;
+}
