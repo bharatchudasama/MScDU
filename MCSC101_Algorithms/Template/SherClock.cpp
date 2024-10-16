74663c439e45e01b4ee799b875af6ce1d884f8ba
Jishnu Bandyopadhyay <therealjishnu@gmail.com>
2024-10-16 20:08:51 +0530

    Create SherClock.cpp
    Simple template to time your function

---
+++ MCSC101_Algorithms/Template/SherClock.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    // Take Input
+    // -----
+    auto start = high_resolution_clock::now();
+
+    ans = yourFunction();  // Call the function you want
+    auto stop = high_resolution_clock::now();
+    cout << "Judgement of Chronos : " auto duration = duration_cast<microseconds>(stop - start) << endl;
+}
+
+int main() {
+	// your code goes here
+	ll t=1;
+	// cin>>t;
+	while(t--){
+	    solve();
+	    if(t) cout << endl;
+	}
+	
+	return 0;
+}
